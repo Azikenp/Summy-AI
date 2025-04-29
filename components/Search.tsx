cd6915bb65b4052eae50f6ff2fdf20917ccf7398
Azikenp <azikenp26@gmail.com>
2025-04-29 14:54:29 +0100

    Refactor Search component to use Textarea for input; implement auto-resize functionality and update form structure

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,25 +3,40 @@
 import { IoSendOutline } from "react-icons/io5";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Textarea } from "./ui/textarea";
 
 const Search = () => {
   const [text, setText] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const autoResize = () => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    }
+  };
+
+  useEffect(() => {
+    autoResize();
+  }, [text]);
 
   return (
-    <div className="fixed bottom-10 border-1 rounded-md">
+    <form className="fixed bottom-10 border-1 rounded-md">
       <div className="flex items-center justify-center ">
-        <Input
+        <Textarea
+          ref={textareaRef}
           placeholder="Enter a search text"
-          className="w-[200px] md:w-[480px] placeholder:text-[10px] bg-transparent"
+          className="w-[200px] h-auto md:w-[480px] placeholder:text-[10px] bg-transparent resize-none overflow-hidden"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <Button>
+        <Button type="submit">
           <IoSendOutline />
         </Button>
       </div>
-    </div>
+    </form>
   );
 };
 
