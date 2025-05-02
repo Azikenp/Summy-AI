2eb6f867bbbdc90786d5f8aadcb5292188390185
Azikenp <azikenp26@gmail.com>
2025-05-02 16:54:22 +0100

    Remove unused chat data from ChatView component

--- components/ChatView.tsx
+++ components/ChatView.tsx
@@ -5,16 +5,6 @@ const ChatView = () => {
   const { history, loading } = useAppSelector((state) => state.gemini);
   console.log(history);
 
-  const chats = [
-    {
-      name: "jne db wedjdk w j j dw jdwjdnwjwj jw dw djwq j djq wdjqd j sdj sj j jj  hd wjdw j",
-    },
-    {
-      name: "jne db wedjdk w j j dw jdwjdnwjwj jw dw djwq j djq wdjqd j sdj sj j jj  hd wjdw jjne db wedjdk w j j dw jdwjdnwjwj jw dw djwq j djq wdjqd j sdj sj j jj  hd wjdw jjne db wedjdk w j j dw jdwjdnwjwj jw dw djwq j djq wdjqd j sdj sj j jj  hd wjdw jjne db wedjdk w j j dw jdwjdnwjwj jw dw djwq j djq wdjqd j sdj sj j jj  hd wjdw jvv j sdj sj j jj  hd wjdw jew wdwdw dwid djqw djq wduw u u",
-      ai: true,
-    },
-  ];
-
   return (
     <div className="mt-2 flex flex-col">
       {history.map((data, i) => (
